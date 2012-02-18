5880a2fa8c2b66c413b7e889ad4fe718ef1a1e46
Inderpal Singh <inderps@thoughtworks.com>
2012-02-18 15:27:08 +0530

    <ips> directory structure creating

---
+++ www/app/app.js
@@ -0,0 +1,11 @@
+Ext.regApplication({
+    name: 'app',
+    launch: function() {
+        this.launched = true;
+        this.mainLaunch();
+    },
+    mainLaunch: function() {
+        //if (!device || !this.launched) {return;}
+        console.log('mainLaunch');
+    }
+});
